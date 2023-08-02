aa26f547f212e91d3d3a6f43c712a3db59bafbab
Milton0123 <maletta.milton@gmail.com>
2023-08-02 19:54:44 -0300

    Create xml Details

---
+++ app/src/main/java/com/example/concesionaria/data/DetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.concesionaria.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.concesionaria.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
