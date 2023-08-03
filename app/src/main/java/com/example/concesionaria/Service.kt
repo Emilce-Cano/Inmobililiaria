481d47a12a6cef296e101a30a370ae50bd7e00a1
Emilce Cano <emilce.cano95@gmail.com>
2023-08-02 23:50:20 -0300

    create GET off Details

--- app/src/main/java/com/example/concesionaria/Service.kt
+++ app/src/main/java/com/example/concesionaria/Service.kt
@@ -1,9 +1,13 @@
 package com.example.concesionaria
 
+import com.example.concesionaria.data.EnvironmentDetailsResponse
 import com.example.concesionaria.data.HousesEnvironmentResponse
 import retrofit2.http.GET
 
 interface Service {
     @GET("/houses")
     suspend fun getRandomHouse(): HousesEnvironmentResponse
+
+    @GET("/houses/environment/detail/{id}")
+    suspend fun getDetailsHouse(): EnvironmentDetailsResponse
 }
