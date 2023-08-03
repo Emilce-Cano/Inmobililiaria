5b7103567409842e436602a35a099519a8248612
Milton0123 <maletta.milton@gmail.com>
2023-08-02 23:56:47 -0300

    create xml details

--- app/src/main/java/com/example/concesionaria/DetailsActivity.kt
+++ app/src/main/java/com/example/concesionaria/DetailsActivity.kt
@@ -1,7 +1,11 @@
-package com.example.concesionaria.data
+package com.example.concesionaria
 
+import Adapterkkk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.concesionaria.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
@@ -10,5 +14,13 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        action()
+    }
+    fun action(){
+        detailsRecyclerView()
+    }
+    fun detailsRecyclerView(){
+        binding.detailsRvGridRooms.layoutManager = GridLayoutManager(this,2)
+        binding.detailsRvGridRooms.adapter = Adapterkkk()
     }
 }
