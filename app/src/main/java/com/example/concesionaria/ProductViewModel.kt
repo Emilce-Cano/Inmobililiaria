511658d7d52be7a79321ab06e266dfc4c9afe01b
jereswaxd <jeresanmartin820@gmail.com>
2023-08-04 10:11:16 -0300

    Merge branch 'Feature/027' of C:\Users\jeres\OneDrive\Escritorio\programacion\curso3\Inmobiliaria with conflicts.

--- app/src/main/java/com/example/concesionaria/ProductViewModel.kt
+++ app/src/main/java/com/example/concesionaria/ProductViewModel.kt
@@ -3,10 +3,10 @@ package com.example.concesionaria
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.concesionaria.data.HousesEnvironmentResponse
+import com.example.concesionaria.data.HousesResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
 
 
 class ProductViewModel(val serviceImp: ServiceImp = ServiceImp()) : ViewModel() {
@@ -15,12 +15,22 @@ class ProductViewModel(val serviceImp: ServiceImp = ServiceImp()) : ViewModel() 
     fun checkNickName(nickName: String) {
         nickNameData.postValue(Utils.checkNickName(nickName))
     }
-    val data = MutableLiveData<HousesEnvironmentResponse>()
 
-    fun getOneHouse() {
+    fun getHouses() {
         CoroutineScope(Dispatchers.IO).launch {
-            val call = serviceImp.getOneHouse()
-            if (call.images != null) {
+            val data = MutableLiveData<HousesResponse>()
+            val call = serviceImp.getHouses()
+            if (call.houses != null) {
+                data.postValue(call)
+            }
+        }
+    }
+
+    fun getEnviroment(id: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val data = MutableLiveData<HousesEnvironmentResponse>()
+            val call = serviceImp.getEnvironment(id)
+            if (call.state != null) {
                 data.postValue(call)
             }
         }
