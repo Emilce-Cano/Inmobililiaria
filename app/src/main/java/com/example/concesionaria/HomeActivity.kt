ed916090fc856f4f2d8739cd130417e81483da45
cristian <cristian_fabian_pesse@hotmail.com>
2023-08-04 16:26:08 -0300

    Realice funciones en homeActivity y deje funcionando la app, a la espera de la creacion de loading y pantalla de error

--- app/src/main/java/com/example/concesionaria/HomeActivity.kt
+++ app/src/main/java/com/example/concesionaria/HomeActivity.kt
@@ -9,6 +9,8 @@ import com.example.concesionaria.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewModel: ProductViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -27,5 +29,33 @@ class HomeActivity : AppCompatActivity() {
     fun goToLogInView() {
         startActivity(Intent(this, LoginActivity::class.java))
     }
-}
 
+    private fun showLoading() {
+        //binding.progressBar.visibility = View.VISIBLE
+        //binding.rvHouseHome.visibility = View.GONE
+        //binding.tvError.visibility = View.GONE
+    }
+
+    private fun calls() {
+        viewModel.getOneHouse()
+    }
+
+    private fun observers() {
+        viewModel.data.observe(this) {
+            //initRecyclerView(it)
+        }
+    }
+
+    private fun showSuccess() {
+        //binding.progressBar.visibility = View.GONE
+        //binding.rvHouseHome.visibility = View.VISIBLE
+        //binding.tvError.visibility = View.GONE
+    }
+
+    private fun showError() {
+        //binding.progressBar.visibility = View.GONE
+        //binding.rvHouseHome.visibility = View.GONE
+        //binding.tvError.visibility = View.VISIBLE
+        //binding.tvError.text = "Error al cargar datos. Inténtalo nuevamente."
+    }
+}
