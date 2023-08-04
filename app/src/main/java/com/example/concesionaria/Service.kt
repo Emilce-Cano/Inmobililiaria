439dfebb6bdc4a66038ef6a914908e1b118e8a41
Emilce Cano <emilce.cano95@gmail.com>
2023-08-04 00:25:07 -0300

    update

--- app/src/main/java/com/example/concesionaria/Service.kt
+++ app/src/main/java/com/example/concesionaria/Service.kt
@@ -2,10 +2,15 @@ package com.example.concesionaria
 
 import com.example.concesionaria.data.HousesEnvironmentResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Service {
     @GET("/houses")
+
     suspend fun getRandomHouse(): HousesEnvironmentResponse
+
     @GET("/houses/environment/{id}")
-    suspend fun getEnvironment(): HousesEnvironmentResponse
+
+    suspend fun getEnvironment(@Path("{id}") id: String): HousesEnvironmentResponse
+
 }
