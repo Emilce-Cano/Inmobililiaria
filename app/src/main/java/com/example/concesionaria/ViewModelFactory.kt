9cfa5775ee79ecc381414f4d472c1c983599a8d6
erik maggio <erik97@hotmail.com.ar>
2023-08-04 13:15:34 -0300

    correcion de errores en factory viewmodel y login , cambio de nick en clases factory y viewmodel, incorporacion de funcion getViewmodel y val lateint en loginactvity

--- app/src/main/java/com/example/concesionaria/ViewModelFactory.kt
+++ app/src/main/java/com/example/concesionaria/ViewModelFactory.kt
@@ -3,8 +3,8 @@ package com.example.concesionaria
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ProductFactory(private var serviceImp: ServiceImp) : ViewModelProvider.Factory {
+class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return ProductViewModel(serviceImp) as T
+        return HomeViewModel() as T
     }
 }
